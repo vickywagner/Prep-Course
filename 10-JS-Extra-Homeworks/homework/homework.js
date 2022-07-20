d5b5d7ad266278db35d3422d5b34b91eed62f4e3
vickywagner <victoriaawagnertrento@gmail.com>
2022-07-20 13:56:57 -0300

    tarea terminada

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,9 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+
+  return Object.entries(objeto);
+  // Object.entries(ºOBJETOaCONVERTIRº) devuelde un array con propiedades y valores (un array por propiedad) // termina siendo un array de array.
 }
 
 
@@ -18,6 +21,31 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+
+
+  // con 'for'
+  var resultado ={};
+  for (let i = 0; i < string.length; i++) {
+    if (resultado[string[i]]){
+      resultado[string[i]] ++;
+    }else{
+      resultado[string[i]] = 1;
+    }
+  }
+  return resultado;
+
+  
+  // con 'reduce'
+  // var arrayString = string.split('');
+  // var resultado = arrayString.reduce(function(acc,elmento){
+  //   if (acc[elmento]){
+  //     acc[elmento] ++;
+  //   }else{
+  //     acc[elmento]= 1;
+  //   }
+  //   return acc;
+  // },{});
+  // return resultado;
 }
 
 
@@ -26,6 +54,16 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  var resultadoUp = '';
+  var resulatdoDown ='';
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] == s[i].toUpperCase()){
+        resultadoUp += s[i];
+    }else{
+      resulatdoDown +=s[i];
+    }    
+  }
+  return resultadoUp + resulatdoDown;  
 }
 
 
@@ -35,6 +73,22 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+
+
+  // metodo slpit() separa una cadena en un array definiendo que caracter los separa (' ') => quita espacios // ('') => no quita nada separa uno por uno
+  // metodo reverse() voltea un array el elemento 1 pasa a ser el ultimo 
+  // metodo join() junta un array definiendo el elemento que quedara entre ellos (' ') => agrega el espacio // ('')=> no agrega nada
+  // con 'for'
+  var array = str.split(' ');
+  for (let i = 0; i < array.length; i++) {
+    var arr = array[i].split('');
+    arr.reverse()
+    array[i] = arr.join('');
+    if(i !== array.length -1){
+      array[i] += ' ';
+    }
+  }
+  return array.join('');
 } 
 
 
@@ -43,6 +97,14 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+
+
+  // metodo toString() transforma a cadena un int
+  var numeroreverse = numero.toString().split('').reverse().join('');
+  if (numero == numeroreverse){
+    return 'Es capicua';
+  }
+  return 'No es capicua'
 }
 
 
@@ -57,6 +119,26 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+
+
+   // quito el mas grande y lo guardo al inicio de un array  repito hasta terminar el array;
+  // // metodo sort() 
+  //   return arr.sort(function(elementoA,elementoB){
+  //     return elementoA.length - elementoB.length;
+  //   });
+
+  // metodo sin sort() PD: se pudo jajaj
+  var arrLength = arr.length;
+  var newArray = [];
+  while(arrLength >0){
+    var sortArr = arr.reduce(function(acc,elemento){
+      return acc.length > elemento.length ? acc : elemento;
+    }); // devuelve palabra mas larga
+    newArray.unshift(sortArr);
+    arr.splice([arr.indexOf(sortArr)],1);
+    arrLength--;
+  }
+    return newArray;
 }
 
 
@@ -66,6 +148,16 @@ function buscoInterseccion(arreglo1, arreglo2){
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+
+  var resultado = [];
+  for (let i = 0; i < arreglo1.length; i++) {
+    for (let j = 0; j < arreglo2.length; j++) {
+      if (arreglo1[i] == arreglo2[j]){
+        resultado.push(arreglo1[i]);
+      }      
+    }    
+  }
+  return resultado;
 }
 
 
